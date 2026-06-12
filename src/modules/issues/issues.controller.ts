50be07f8f30a295257d1dbac698a422e63b8a4fe
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-12 23:39:34 +0600

    code added

--- src/modules/issues/issues.controller.ts
+++ src/modules/issues/issues.controller.ts
@@ -1,5 +1,5 @@
 // src/modules/issues/issues.controller.ts
-// import type { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { issueServices } from './issues.service';
 import { sendSuccess, sendError } from '../../utils/response';
