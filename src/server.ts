ae5c322968ec9c734fac06fe0e814745ca5429ef
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-13 23:39:10 +0600

    code added

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-// src/server.ts
-import app from './app';
-import { pool, initDatabase } from './config/db';
+// // src/server.ts
+// import app from './app';
+// import { pool, initDatabase } from './config/db';
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-const startServer = async () => {
-    try {
+// const startServer = async () => {
+//     try {
        
-        await initDatabase();
-        console.log('✅ Database tables ready');
+//         await initDatabase();
+//         console.log('✅ Database tables ready');
         
         
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error('❌ Failed to start server:', error);
-        process.exit(1);
-    }
-};
+//         app.listen(PORT, () => {
+//             console.log(`🚀 Server running on http://localhost:${PORT}`);
+//         });
+//     } catch (error) {
+//         console.error('❌ Failed to start server:', error);
+//         process.exit(1);
+//     }
+// };
 
-startServer();
+// startServer();
