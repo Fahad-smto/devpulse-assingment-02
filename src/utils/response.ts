8cfaaa07decb8d6d76bc5a07f90f076f7c3a2e74
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-14 23:23:55 +0600

    code added

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -14,15 +14,15 @@ export const sendSuccess = (
     });
 };
 
-export const sendError = (
-    res: Response,
-    statusCode: number,
-    message: string,
-    errors?: unknown
-) => {
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        errors,
-    });
-};
+// export const sendError = (
+//     res: Response,
+//     statusCode: number,
+//     message: string,
+//     errors?: unknown
+// ) => {
+//     return res.status(statusCode).json({
+//         success: false,
+//         message,
+//         errors,
+//     });
+// };
